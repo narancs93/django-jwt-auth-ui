eac1b88901c814e15d345768c2e9ca46e3ce85b4
Lajos Seyler <lajos.seyler@gmail.com>
2024-11-01 23:11:01 +0100

    register fields on registerForm using react-hook-form

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,20 +1,52 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 import styled from "styled-components";
 
 const StyledSignupForm = styled.form`
   gap: 1.25rem;
 `;
 
+type Inputs = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 function SignupForm() {
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data, event) => {
+    event?.preventDefault();
+    console.log(data);
+  };
+
   return (
-    <StyledSignupForm className="uk-flex uk-flex-column">
-      <input className="uk-input" type="email" placeholder="Email" />
-      <input className="uk-input" type="password" placeholder="Password" />
+    <StyledSignupForm
+      className="uk-flex uk-flex-column"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <input
+        className="uk-input"
+        type="email"
+        placeholder="Email"
+        {...register("email")}
+      />
+      <input
+        className="uk-input"
+        type="password"
+        placeholder="Password"
+        {...register("password")}
+      />
       <input
         className="uk-input"
         type="password"
         placeholder="Confirm Password"
       />
-      <button className="uk-button uk-button-primary">Signup</button>
+      <button
+        className="uk-button uk-button-primary"
+        {...register("confirmPassword")}
+      >
+        Signup
+      </button>
     </StyledSignupForm>
   );
 }
