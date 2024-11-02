39082a414f4075fba9da0bd8cbd13d9438966acc
Lajos Seyler <lajos.seyler@gmail.com>
2024-11-02 11:14:42 +0100

    handle form submission for registration

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,23 +1,32 @@
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import { SubmitHandler, useForm } from "react-hook-form";
 import styled from "styled-components";
+import SignupData from "../interfaces/SignupData";
+import SignupErrorResponse from "../interfaces/SignupErrorResponse";
+import { signup } from "../services/authService";
 
 const StyledSignupForm = styled.form`
   gap: 1.25rem;
 `;
 
-type Inputs = {
-  email: string;
-  password: string;
-  confirmPassword: string;
-};
-
 function SignupForm() {
-  const { register, handleSubmit } = useForm<Inputs>();
+  const { register, handleSubmit } = useForm<SignupData>();
 
-  const onSubmit: SubmitHandler<Inputs> = (data, event) => {
+  const mutation = useMutation({
+    mutationFn: signup,
+    onSuccess: (data) => {
+      console.log(data);
+    },
+  });
+
+  const onSubmit: SubmitHandler<SignupData> = (data, event) => {
     event?.preventDefault();
-    console.log(data);
+    mutation.mutate(data);
   };
+
+  const signupErrors = (mutation.error as AxiosError<SignupErrorResponse>)
+    ?.response?.data;
 
   return (
     <StyledSignupForm
@@ -25,28 +34,41 @@ function SignupForm() {
       onSubmit={handleSubmit(onSubmit)}
     >
       <input
-        className="uk-input"
+        className={`uk-input ${signupErrors?.email && "uk-form-danger"}`}
         type="email"
         placeholder="Email"
         {...register("email")}
       />
+      {signupErrors?.email && (
+        <div className="uk-alert-danger uk-padding-small">
+          {signupErrors?.email}
+        </div>
+      )}
       <input
-        className="uk-input"
+        className={`uk-input ${signupErrors?.password && "uk-form-danger"}`}
         type="password"
         placeholder="Password"
         {...register("password")}
       />
+      {signupErrors?.password && (
+        <div className="uk-alert-danger uk-padding-small">
+          {signupErrors?.password}
+        </div>
+      )}
       <input
-        className="uk-input"
+        className={`uk-input ${
+          signupErrors?.confirm_password && "uk-form-danger"
+        }`}
         type="password"
         placeholder="Confirm Password"
+        {...register("confirm_password")}
       />
-      <button
-        className="uk-button uk-button-primary"
-        {...register("confirmPassword")}
-      >
-        Signup
-      </button>
+      {signupErrors?.confirm_password && (
+        <div className="uk-alert-danger uk-padding-small">
+          {signupErrors?.confirm_password}
+        </div>
+      )}
+      <button className="uk-button uk-button-primary">Signup</button>
     </StyledSignupForm>
   );
 }
